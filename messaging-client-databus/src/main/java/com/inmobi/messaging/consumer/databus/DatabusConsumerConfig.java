95843000235a5870fa32d0327d31255e1a99c146
Amareshwari Sriramadasu <amareshwari.sr@inmobi.com>
2012-05-28 15:04:28 +0530

    Fix typo in config name

--- messaging-client-databus/src/main/java/com/inmobi/messaging/consumer/databus/DatabusConsumerConfig.java
+++ messaging-client-databus/src/main/java/com/inmobi/messaging/consumer/databus/DatabusConsumerConfig.java
@@ -2,6 +2,9 @@ package com.inmobi.messaging.consumer.databus;
 
 import com.inmobi.databus.FSCheckpointProvider;
 
+/**
+ * Configuration properties and their default values for {@link DatabusConsumer}
+ */
 public interface DatabusConsumerConfig {
 
   public static final String queueSizeConfig = "databus.consumer.buffer.size";
@@ -21,7 +24,7 @@ public interface DatabusConsumerConfig {
   public static final String databusClustersConfig = "databus.consumer.clusters";
   
   public static final String databusChkProviderConfig = 
-      "databus.consumer.chkprovider.classname";
+      "databus.consumer.chkpoint.provider.classname";
   public static final String DEFAULT_CHK_PROVIDER = FSCheckpointProvider.class
   .getName();
 
