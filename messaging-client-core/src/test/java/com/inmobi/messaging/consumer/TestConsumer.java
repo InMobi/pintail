29c1cfe62656dc7ee532ba2c2846bb398bce3b4c
Amareshwari Sriramadasu <amareshwari.sr@inmobi.com>
2012-05-28 11:41:35 +0530

    Add factory methods to take startTime as parameter

--- messaging-client-core/src/test/java/com/inmobi/messaging/consumer/TestConsumer.java
+++ messaging-client-core/src/test/java/com/inmobi/messaging/consumer/TestConsumer.java
@@ -21,7 +21,7 @@ public class TestConsumer {
     conf.set(MessageConsumerFactory.CONSUMER_NAME_KEY, "testconsumer");
     AbstractMessageConsumer consumer =
         (AbstractMessageConsumer) MessageConsumerFactory.create(conf);
-    doTest(consumer);
+    doTest(consumer, null);
     Assert.assertNull(consumer.getStartTime());
   }
 
@@ -29,8 +29,7 @@ public class TestConsumer {
   public void testLoadFromClasspath() {
     AbstractMessageConsumer consumer =
         (AbstractMessageConsumer) MessageConsumerFactory.create();
-    doTest(consumer);
-    Assert.assertNull(consumer.getStartTime());
+    doTest(consumer, null);
   }
 
   @Test
@@ -40,8 +39,7 @@ public class TestConsumer {
     AbstractMessageConsumer consumer =
         (AbstractMessageConsumer) MessageConsumerFactory.create(
             url.getFile());
-    doTest(consumer);
-    Assert.assertNull(consumer.getStartTime());
+    doTest(consumer, null);
   }
 
   @Test
@@ -52,8 +50,7 @@ public class TestConsumer {
     AbstractMessageConsumer consumer = 
       (AbstractMessageConsumer) MessageConsumerFactory.create(
           conf, MockConsumer.class.getName());
-    doTest(consumer);
-    Assert.assertNull(consumer.getStartTime());
+    doTest(consumer, null);
   }
 
   @Test
@@ -62,8 +59,56 @@ public class TestConsumer {
       (AbstractMessageConsumer) MessageConsumerFactory.create(
           new ClientConfig(), MockConsumer.class.getName(), "test",
           "testconsumer");
-    doTest(consumer);
-    Assert.assertNull(consumer.getStartTime());
+    doTest(consumer, null);
+  }
+
+  @Test
+  public void testWithStartTime() {
+    ClientConfig conf = new ClientConfig();
+    conf.set(MessageConsumerFactory.CONSUMER_CLASS_NAME_KEY,
+        MockConsumer.class.getName());
+    conf.set(MessageConsumerFactory.TOPIC_NAME_KEY, "test");
+    conf.set(MessageConsumerFactory.CONSUMER_NAME_KEY, "testconsumer");
+    AbstractMessageConsumer consumer =
+        (AbstractMessageConsumer) MessageConsumerFactory.create(conf, now);
+    doTest(consumer, now);
+  }
+
+  @Test
+  public void testLoadFromClasspathWithStartTime() {
+    AbstractMessageConsumer consumer =
+        (AbstractMessageConsumer) MessageConsumerFactory.create(now);
+    doTest(consumer, now);
+  }
+
+  @Test
+  public void testLoadFromFileNameWithStartTime() {
+    URL url = getClass().getClassLoader().getResource(
+        MessageConsumerFactory.MESSAGE_CLIENT_CONF_FILE);
+    AbstractMessageConsumer consumer =
+        (AbstractMessageConsumer) MessageConsumerFactory.create(
+            url.getFile(), now);
+    doTest(consumer, now);
+  }
+
+  @Test
+  public void testLoadFromClassNameWithStartTime() {
+    ClientConfig conf = new ClientConfig();
+    conf.set(MessageConsumerFactory.TOPIC_NAME_KEY, "test");
+    conf.set(MessageConsumerFactory.CONSUMER_NAME_KEY, "testconsumer");
+    AbstractMessageConsumer consumer = 
+      (AbstractMessageConsumer) MessageConsumerFactory.create(
+          conf, MockConsumer.class.getName(), now);
+    doTest(consumer, now);
+  }
+
+  @Test
+  public void testTopicNConsumerNameWithStartTime() {
+    AbstractMessageConsumer consumer = 
+      (AbstractMessageConsumer) MessageConsumerFactory.create(
+          new ClientConfig(), MockConsumer.class.getName(), "test",
+          "testconsumer", now);
+    doTest(consumer, now);
   }
 
   @Test
@@ -72,16 +117,16 @@ public class TestConsumer {
       (AbstractMessageConsumer) MessageConsumerFactory.create(
           new ClientConfig(), MockConsumer.class.getName(), "test",
           "testconsumer", now);
-    doTest(consumer);
-    Assert.assertEquals(consumer.getStartTime(), now);
+    doTest(consumer, now);
   }
 
-  private void doTest(AbstractMessageConsumer consumer) {
+  private void doTest(AbstractMessageConsumer consumer, Date startTime) {
     Assert.assertTrue(consumer instanceof MockConsumer);
     Assert.assertFalse(consumer.isMarkSupported());
     Assert.assertTrue(((MockConsumer)consumer).initedConf);
     Assert.assertEquals(consumer.getTopicName(), "test");
     Assert.assertEquals(consumer.getConsumerName(), "testconsumer");
+    Assert.assertEquals(consumer.getStartTime(), startTime);
     
     Message msg = consumer.next();
     consumer.close();
