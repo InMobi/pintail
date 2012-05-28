33eda997433c215942fd610358d7fed1714ebfdb
Amareshwari Sriramadasu <amareshwari.sr@inmobi.com>
2012-05-28 14:31:06 +0530

    Move databus consumer configuration to DatabusConsumerConfig

---
+++ messaging-client-databus/src/main/java/com/inmobi/messaging/consumer/databus/DatabusConsumerConfig.java
@@ -0,0 +1,28 @@
+package com.inmobi.messaging.consumer.databus;
+
+import com.inmobi.databus.FSCheckpointProvider;
+
+public interface DatabusConsumerConfig {
+
+  public static final String queueSizeConfig = "databus.consumer.buffer.size";
+  public static final int DEFAULT_QUEUE_SIZE = 5000;
+  
+  public static final String waitTimeForFlushConfig = 
+  "databus.consumer.waittime.forcollectorflush";
+  public static final long DEFAULT_WAIT_TIME_FOR_FLUSH = 1000; // 1 second
+  
+  public static final String checkpointDirConfig = 
+  "databus.consumer.checkpoint.dir";
+  public static final String DEFAULT_CHECKPOINT_DIR = ".";
+  
+  public static final String databusConfigFileKey = "databus.conf";
+  public static final String DEFAULT_DATABUS_CONFIG_FILE = "databus.xml";
+
+  public static final String databusClustersConfig = "databus.consumer.clusters";
+  
+  public static final String databusChkProviderConfig = 
+      "databus.consumer.chkprovider.classname";
+  public static final String DEFAULT_CHK_PROVIDER = FSCheckpointProvider.class
+  .getName();
+
+}
