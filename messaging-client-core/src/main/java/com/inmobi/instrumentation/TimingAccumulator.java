6f4370637020d6e2db5bde2df23283b26dacf8f4
Amareshwari Sriramadasu <amareshwari.sr@inmobi.com>
2012-10-08 11:37:37 +0530

    Fix indentation in TimingAccumulator

--- messaging-client-core/src/main/java/com/inmobi/instrumentation/TimingAccumulator.java
+++ messaging-client-core/src/main/java/com/inmobi/instrumentation/TimingAccumulator.java
@@ -14,162 +14,144 @@ import java.util.concurrent.atomic.AtomicLong;
  * spent under normal circumstances i.e. free of unhandled exceptions
  */
 public class TimingAccumulator {
-    
-	private final AtomicLong invocationCount = new AtomicLong(0);
-	
-	private final AtomicLong cumulativeNanoseconds = new AtomicLong(0);
-	
-	public enum Outcome
-	{
-		SUCCESS,
-		GRACEFUL_FAILURE,
-		UNHANDLED_FAILURE
-	}
 
-	private final AtomicLong successCount = new AtomicLong(0);
-	private final AtomicLong gracefulTerminates = new AtomicLong(0);	
-	private final AtomicLong failureCount = new AtomicLong(0);
-	
-    /**
-     * The number of times something was invoked.
-     * Increment this counter at entry.
+  private final AtomicLong invocationCount = new AtomicLong(0);
+
+  private final AtomicLong cumulativeNanoseconds = new AtomicLong(0);
+
+  public enum Outcome {
+    SUCCESS,
+    GRACEFUL_FAILURE,
+    UNHANDLED_FAILURE
+  }
+
+  private final AtomicLong successCount = new AtomicLong(0);
+  private final AtomicLong gracefulTerminates = new AtomicLong(0);	
+  private final AtomicLong failureCount = new AtomicLong(0);
+
+  /**
+   * The number of times something was invoked.
+   * Increment this counter at entry.
+   */
+  public void accumulateInvocation() {
+    invocationCount.incrementAndGet();
+  }
+
+  public long accumulateInvocationStartTimer() {
+    long r = System.nanoTime();
+    invocationCount.incrementAndGet();
+    return r;
+  }
+
+  /**
+   * Accumulator for time spent in a call
+   * Usually incremented only on successful returns
+   */
+  private void accumulateSuccess() {
+    successCount.incrementAndGet();
+  }
+
+  private void accumulateFailure() {
+    failureCount.incrementAndGet();
+  }
+
+  /**
+   * The number of times something returned without an unhandled exception.
+   * Increment this count at exit
+   */
+  private void accumulateTimeSpent(long nanos) {
+    cumulativeNanoseconds.addAndGet(nanos);
+  }
+
+  private void accumulateGracefulTerminates() {
+    gracefulTerminates.incrementAndGet();
+  }
+
+  public void accumulateOutcomeWithDelta(Outcome o, long delta) {
+    accumulateOutcome(o);
+    accumulateTimeSpent(delta);    	
+  }
+
+  public void accumulateOutcome(Outcome o, long startTime) {
+    accumulateOutcome(o);
+    long e = System.nanoTime();
+    accumulateTimeSpent(e - startTime);    	
+  }
+
+  private void accumulateOutcome(Outcome o) {
+    switch(o) {
+    case SUCCESS:
+      accumulateSuccess();
+      break;
+    case GRACEFUL_FAILURE:
+      accumulateGracefulTerminates();
+      break;
+    case UNHANDLED_FAILURE:
+      accumulateFailure();
+      break;
+    }
+  }
+
+  public long getInvocationCount() {
+    return invocationCount.get();
+  }
+
+  public long getSuccessCount() {
+    return successCount.get();
+  }
+
+  public long getCumulativeNanoseconds() {
+    return cumulativeNanoseconds.get();
+  }
+
+  public long getUnhandledExceptionCount() {
+    return failureCount.get();
+  }
+
+  public long getGracefulTerminates() {
+    return gracefulTerminates.get();
+  }
+
+  public long getInFlight() {
+    /* We can either choose to maintain yet another variable
+     * for counting any form of returns or add all the return counts.
+     * 
+     * Having another variable implies yet another atomic increment
+     * Not having implies a sloppy answer.
+     * 
+     * We choose the latter since by definition, this is a shaky metric.
+     * 
+     * As long as callers code accumulateInvocation() with exactly one
+     * accumulateOutcome()/accumulateOutcomeDelta() following it, the
+     * result shall remain non-negative.
+     * 
+     * Since this is a gauge and not a running counter, by definition,
+     * the values is allowed to fluctuate across readings in a busy system
      */
-	public void accumulateInvocation()
-	{
-		invocationCount.incrementAndGet();
-	}
-	
-	public long accumulateInvocationStartTimer()
-	{
-		long r = System.nanoTime();
-		invocationCount.incrementAndGet();
-		return r;
-	}
-	
-	/**
-	 * Accumulator for time spent in a call
-	 * Usually incremented only on successful returns
-	 */
-	private void accumulateSuccess()
-	{
-		successCount.incrementAndGet();
-	}
-	
-	private void accumulateFailure()
-	{
-		failureCount.incrementAndGet();
-	}
-	
-	/**
-	 * The number of times something returned without an unhandled exception.
-	 * Increment this count at exit
-	 */
-	private void accumulateTimeSpent(long nanos)
-	{
-		cumulativeNanoseconds.addAndGet(nanos);
-	}
-	
-    private void accumulateGracefulTerminates()
-    {
-        gracefulTerminates.incrementAndGet();
-    }
- 
-    public void accumulateOutcomeWithDelta(Outcome o, long delta)
-    {
-    	accumulateOutcome(o);
-        accumulateTimeSpent(delta);    	
-    }
-    
-    public void accumulateOutcome(Outcome o, long startTime)
-    {
-    	accumulateOutcome(o);
-        long e = System.nanoTime();
-        accumulateTimeSpent(e - startTime);    	
-    }
-    
-    private void accumulateOutcome(Outcome o)
-    {
-    	switch(o)
-    	{
-    	case SUCCESS:
-    		accumulateSuccess();
-    		break;
-    	case GRACEFUL_FAILURE:
-    		accumulateGracefulTerminates();
-    		break;
-    	case UNHANDLED_FAILURE:
-    		accumulateFailure();
-    		break;
-    	}
-    }
-    
-	public long getInvocationCount()
-	{
-		return invocationCount.get();
-	}
-	
-	public long getSuccessCount()
-	{
-		return successCount.get();
-	}
-	
-	public long getCumulativeNanoseconds()
-	{
-		return cumulativeNanoseconds.get();
-	}
-	
-	public long getUnhandledExceptionCount()
-	{
-	    return failureCount.get();
-	}
 
-    public long getGracefulTerminates()
-    {
-        return gracefulTerminates.get();
-    }
-    
-    public long getInFlight()
-    {
-    	/* We can either choose to maintain yet another variable
-    	 * for counting any form of returns or add all the return counts.
-    	 * 
-    	 * Having another variable implies yet another atomic increment
-    	 * Not having implies a sloppy answer.
-    	 * 
-    	 * We choose the latter since by definition, this is a shaky metric.
-    	 * 
-    	 * As long as callers code accumulateInvocation() with exactly one
-    	 * accumulateOutcome()/accumulateOutcomeDelta() following it, the
-    	 * result shall remain non-negative.
-    	 * 
-    	 * Since this is a gauge and not a running counter, by definition,
-    	 * the values is allowed to fluctuate across readings in a busy system
-    	 */
+    return getInvocationCount() - (getSuccessCount() + getGracefulTerminates()
+        + getUnhandledExceptionCount());
+  }
 
-    	return getInvocationCount() - 
-    		(getSuccessCount() + getGracefulTerminates() + getUnhandledExceptionCount());
-    }
+  @Override
+  public String toString() {
+    return String.format(" {\"nanos\": %d, \"invocations\": %d, \"success\": " +
+    		"%d, \"failures\": %d, \"terminates\": %d, " +
+        "\"in-flight\": %d  } ",
+        getCumulativeNanoseconds(), getInvocationCount(), getSuccessCount(),
+        getUnhandledExceptionCount(),getGracefulTerminates(),
+        getInFlight());
+  }
 
-    @Override
-    public String toString()
-    {
-        return String.format(" {\"nanos\": %d, \"invocations\": %d, \"success\": %d, " +
-        		"\"failures\": %d, \"terminates\": %d, " +
-        		"\"in-flight\": %d  } ",
-            getCumulativeNanoseconds(), getInvocationCount(), getSuccessCount(),
-            getUnhandledExceptionCount(),getGracefulTerminates(),
-            getInFlight());
-    }
-    
-    public Map<String, Number> getMap() {
-      HashMap<String, Number> hash = new HashMap<String, Number>();
-      hash.put("cumulativeNanoseconds", getCumulativeNanoseconds());
-      hash.put("invocationCount", getInvocationCount());
-      hash.put("successCount", getSuccessCount());
-      hash.put("unhandledExceptionCount",
-          getUnhandledExceptionCount());
-      hash.put("gracefulTerminates", getGracefulTerminates());
-      hash.put("inFlight", getInFlight());
-      return hash;
-    }
+  public Map<String, Number> getMap() {
+    HashMap<String, Number> hash = new HashMap<String, Number>();
+    hash.put("cumulativeNanoseconds", getCumulativeNanoseconds());
+    hash.put("invocationCount", getInvocationCount());
+    hash.put("successCount", getSuccessCount());
+    hash.put("unhandledExceptionCount",
+        getUnhandledExceptionCount());
+    hash.put("gracefulTerminates", getGracefulTerminates());
+    hash.put("inFlight", getInFlight());
+    return hash;
+  }
 }
