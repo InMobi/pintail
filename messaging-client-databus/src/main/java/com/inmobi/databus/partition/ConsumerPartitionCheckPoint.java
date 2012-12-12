9516a6182ae7fc80b03041c6ed62aa21ec9fc857
Sreekanth Ramakrishnan <sreekanth.ramakrishnan@inmobi.com>
2012-12-12 16:02:25 +0530

    Optimization for Reducing Message Memory Foot Print

---
+++ messaging-client-databus/src/main/java/com/inmobi/databus/partition/ConsumerPartitionCheckPoint.java
@@ -0,0 +1,11 @@
+package com.inmobi.databus.partition;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sreekanth.ramakrishn
+ * Date: 12/12/12
+ * Time: 10:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConsumerPartitionCheckPoint {
+}
