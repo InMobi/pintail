d607e78409d28e43b68e91af556eb25b16f728ea
rohit.kochar <rohit.kochar@inmobi.com>
2013-02-20 15:16:13 +0530

    fixing corner case which was causing TC to fail on jenkins

--- messaging-client-core/src/test/java/com/inmobi/messaging/consumer/audit/TestAuditStatsQuery.java
+++ messaging-client-core/src/test/java/com/inmobi/messaging/consumer/audit/TestAuditStatsQuery.java
@@ -14,7 +14,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.inmobi.messaging.ClientConfig;
 import com.inmobi.messaging.Message;
 import com.inmobi.messaging.consumer.MessageConsumer;
 import com.inmobi.messaging.consumer.MockInMemoryConsumer;
@@ -28,6 +27,8 @@ public class TestAuditStatsQuery {
   private MessagePublisher publisher;
   private String startTime, endTime;
   private Date startDate, endDate;
+  private SimpleDateFormat formatter = new SimpleDateFormat(
+      AuditUtil.DATE_FORMAT);
 
   private int totalData = 10;// put an even no.
   String topic = "topic1";
@@ -53,7 +54,6 @@ public class TestAuditStatsQuery {
   private void generateData(MessagePublisher publisher, String topic,
       String topic1) {
     String msg = "sample data";
-    SimpleDateFormat formatter = new SimpleDateFormat(AuditUtil.DATE_FORMAT);
     startDate = new Date();
     Calendar calendar = Calendar.getInstance();
     calendar.setTime(startDate);
@@ -174,12 +174,21 @@ public class TestAuditStatsQuery {
   @Test
   public void testAuditQueryValidCuttoffTime() throws IOException,
       InterruptedException, TException, ParseException {
-    ClientConfig config =
-        ClientConfig.load("src/test/resources/mock-publisher.properties");
-    publisher = MessagePublisherFactory.create(config);
     assert (publisher instanceof com.inmobi.messaging.publisher.MockInMemoryPublisher);
     generateData(topic2, topic3);
-    System.out.println(startTime + " " + endTime);
+    System.out.println("Before audit has been generated: StartTime "
+        + startTime + "EndTime " + endTime);
+    int i = 0;
+    while (!((MockInMemoryPublisher) publisher).source
+        .containsKey(AuditUtil.AUDIT_STREAM_TOPIC_NAME) && i < 5) {
+      Thread.sleep(2500);
+      i++;
+    }
+    assert (i != 5);// to check whether audit was generated or not
+    Date endDate = new Date();
+    String endTime = formatter.format(endDate);
+    System.out.println("After audit was generated: StartTime " + startTime
+        + "EndTime " + endTime);
     AuditStatsQuery query =
         new AuditStatsQuery("mock", endTime, startTime, "topic=" + topic2,
             null, null, "1", null);
@@ -189,8 +198,6 @@ public class TestAuditStatsQuery {
     Thread.sleep(62000);
     generateData(topic2, topic3);
     publisher.close();
-    // query.filter = new Filter("topic=" + topic2);
-    // query.groupBy = new GroupBy(null);
     MessageConsumer consumer = query.getConsumer(startDate, "mock");
     ((MockInMemoryConsumer) consumer)
         .setSource(((MockInMemoryPublisher) (publisher)).source);
@@ -198,6 +205,8 @@ public class TestAuditStatsQuery {
     Collection<Long> sent = query.getReceived().values();
     assert (sent.iterator().hasNext());
     Long sentPublisher = sent.iterator().next();
+    System.out.println("Total Sent by Publisher " + sentPublisher
+        + "and expecting " + totalData / 2);
     assert (sentPublisher == totalData / 2);
 
   }
@@ -205,9 +214,6 @@ public class TestAuditStatsQuery {
   @Test
   public void testAuditQueryMaxMessages() throws IOException,
       InterruptedException, TException, ParseException {
-    ClientConfig config =
-        ClientConfig.load("src/test/resources/mock-publisher.properties");
-    publisher = MessagePublisherFactory.create(config);
     assert (publisher instanceof com.inmobi.messaging.publisher.MockInMemoryPublisher);
     generateData(topic4, topic5);
     // setting very large timeout so that query cuttoff should happen as per max
