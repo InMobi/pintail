b31b6661d596fa690ea1adb13076065b3eaa5387
raju.bairishetti <raju.bairishetti@inmobi.com>
2013-04-17 16:22:04 +0530

    changed the log

--- messaging-client-databus/src/main/java/com/inmobi/messaging/consumer/databus/AbstractMessagingDatabusConsumer.java
+++ messaging-client-databus/src/main/java/com/inmobi/messaging/consumer/databus/AbstractMessagingDatabusConsumer.java
@@ -133,8 +133,8 @@ public abstract class AbstractMessagingDatabusConsumer
     relativeStartTimeStr = config.getString(relativeStartTimeConfig);
 
     if (relativeStartTimeStr == null && retentionInHours!= null) {
-      LOG.warn("retentionConfig is deprecated." +
-          " Use relativeStartTimeConfig instead");
+      LOG.warn(retentionConfig  + " is deprecated." +
+          " Use " + relativeStartTimeConfig + " instead");
       int minutes = (Integer.parseInt(retentionInHours)) * 60;
       relativeStartTimeStr = String.valueOf(minutes);
     }
