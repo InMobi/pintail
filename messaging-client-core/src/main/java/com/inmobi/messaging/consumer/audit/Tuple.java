9213a7cd1e12a945994d99b43c7acd6e62905ca0
rohitkochar <rohit.kochar@inmobi.com>
2013-05-15 11:58:09 +0530

    adding equals and hashcode

--- messaging-client-core/src/main/java/com/inmobi/messaging/consumer/audit/Tuple.java
+++ messaging-client-core/src/main/java/com/inmobi/messaging/consumer/audit/Tuple.java
@@ -4,23 +4,73 @@ import java.util.Date;
 import java.util.Map;
 
 public class Tuple {
-  private String name;
-  private String tier;
-  private String cluster;
-  private Date timestamp;
+  final private String hostname;
+  final private String tier;
+  final private String cluster;
+  final private Date timestamp;
+  final private String topic;
+  private long sent, received;
+
   private Map<LatencyColumns, Long> latencyCountMap;
 
-  public Tuple(String name, String tier, String cluster, Date timestamp,
+  public Tuple(String hostname, String tier, String cluster, Date timestamp,
+      String topic,
                Map<LatencyColumns, Long> latencyCountMap) {
-    this.name = name;
+    this.hostname = hostname;
     this.tier = tier;
     this.cluster = cluster;
     this.timestamp = timestamp;
+    this.topic = topic;
     this.latencyCountMap = latencyCountMap;
   }
 
-  public String getName() {
-    return name;
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((cluster == null) ? 0 : cluster.hashCode());
+    result = prime * result + ((hostname == null) ? 0 : hostname.hashCode());
+    result = prime * result + ((tier == null) ? 0 : tier.hashCode());
+    result = prime * result + ((timestamp == null) ? 0 : timestamp.hashCode());
+    result = prime * result + ((topic == null) ? 0 : topic.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Tuple other = (Tuple) obj;
+    if (cluster == null) {
+      if (other.cluster != null)
+        return false;
+    } else if (!cluster.equals(other.cluster))
+      return false;
+    if (hostname == null) {
+      if (other.hostname != null)
+        return false;
+    } else if (!hostname.equals(other.hostname))
+      return false;
+    if (tier == null) {
+      if (other.tier != null)
+        return false;
+    } else if (!tier.equals(other.tier))
+      return false;
+    if (timestamp == null) {
+      if (other.timestamp != null)
+        return false;
+    } else if (!timestamp.equals(other.timestamp))
+      return false;
+    if (topic == null) {
+      if (other.topic != null)
+        return false;
+    } else if (!topic.equals(other.topic))
+      return false;
+    return true;
   }
 
   public String getTier() {
@@ -35,6 +85,14 @@ public class Tuple {
     return timestamp;
   }
 
+  public String getHostname() {
+    return hostname;
+  }
+
+  public String getTopic() {
+    return topic;
+  }
+
   public Map<LatencyColumns, Long> getLatencyCountMap() {
     return latencyCountMap;
   }
@@ -46,7 +104,7 @@ public class Tuple {
   @Override
   public String toString() {
     return "Tuple{" +
-        "name='" + name + '\'' +
+        "name='" + hostname + '\'' +
         ", tier='" + tier + '\'' +
         ", cluster='" + cluster + '\'' +
         ", timestamp=" + timestamp +
