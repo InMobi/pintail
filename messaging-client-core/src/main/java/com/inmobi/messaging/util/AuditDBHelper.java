4b6df6e2a11957778ef2c8f895e8c80e3bac6465
Annapurna <annapurna.vmss@inmobi.com>
2013-05-31 16:33:44 +0530

    Column names are not given as part of preparedStatement in AuditDBHelper

--- messaging-client-core/src/main/java/com/inmobi/messaging/util/AuditDBHelper.java
+++ messaging-client-core/src/main/java/com/inmobi/messaging/util/AuditDBHelper.java
@@ -112,8 +112,8 @@ public class AuditDBHelper {
 
   private static String getUpdateStmtForUpdation() {
     String setString = "";
-    for (int i = 0; i < LatencyColumns.values().length; i++) {
-      setString += ", ? = ?";
+    for (LatencyColumns columns : LatencyColumns.values()) {
+      setString += ", "+columns.toString()+" = ?";
     }
     String updateStatement = "update " + AuditDBConstants.TABLE_NAME + " set " +
         "" + AuditDBConstants.SENT + " = ?" + setString + " where " + Column
@@ -125,12 +125,13 @@ public class AuditDBHelper {
   }
 
   private static String getInsertStmtForUpdation() {
-    String columnString = "";
-    for (int i = 0; i < LatencyColumns.values().length; i++) {
+    String columnString = "", columnNames = "";
+    for (LatencyColumns column : LatencyColumns.values()) {
+      columnNames += column.toString() +", ";
       columnString += "?, ";
     }
     String insertStatement =
-        "insert into " + AuditDBConstants.TABLE_NAME + " (" + columnString +
+        "insert into " + AuditDBConstants.TABLE_NAME + " (" + columnNames +
             AuditDBConstants.TIMESTAMP + "," + Column.HOSTNAME +
             ", " + Column.TIER + ", " + Column.TOPIC +
             ", " + Column.CLUSTER + ", " + AuditDBConstants.SENT+ ") values " +
@@ -158,7 +159,6 @@ public class AuditDBHelper {
           tuple.getLatencyCountMap();
       int numberColumns = LatencyColumns.values().length;
       for (LatencyColumns latencyColumn : LatencyColumns.values()) {
-        insertPreparedStatement.setString(index, latencyColumn.toString());
         Long count = latencyCountMap.get(latencyColumn);
         if (count == null)
           count = 0l;
@@ -198,7 +198,6 @@ public class AuditDBHelper {
       int index = 1;
       updatePreparedStatement.setLong(index++, sent);
       for (LatencyColumns latencyColumn : LatencyColumns.values()) {
-        updatePreparedStatement.setString(index++, latencyColumn.toString());
         updatePreparedStatement.setLong(index++, latencyCountMap.get(
             latencyColumn));
       }
@@ -275,7 +274,6 @@ public class AuditDBHelper {
       for (Column column : Column.values()) {
         String value = filter.getFilters().get(column);
         if ( value != null || !value.isEmpty() ) {
-          preparedstatement.setString(index++, column.toString());
           preparedstatement.setString(index++, value);
         }
       }
@@ -335,8 +333,11 @@ public class AuditDBHelper {
       sumString += ", Sum(" + latencyColumn.toString() + ")";
       asString += ", " + latencyColumn.toString();
     }
-    for (int i = 0; i < filter.getFilters().size(); i++) {
-      whereString += " and ? = ?";
+    for (Column column : Column.values()) {
+      String value = filter.getFilters().get(column);
+      if ( value != null || !value.isEmpty() ) {
+        whereString += " and " + column.toString() +" = ?";
+      }
     }
     for (Column column : groupBy.getGroupByColumns()) {
       if (!groupByString.isEmpty()) {
