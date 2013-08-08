c0ae70cebee5c2113970f4b3274126ba73690838
raju.bairishetti <raju.bairishetti@inmobi.com>
2013-08-08 13:54:17 +0530

    Revert "close the socket to make the port number available to others"
    This reverts commit dff9376b084f032e74a2f60e8a0b8da1a66c34e7.

--- messaging-client-scribe/src/test/java/com/inmobi/messaging/PortNumberUtil.java
+++ messaging-client-scribe/src/test/java/com/inmobi/messaging/PortNumberUtil.java
@@ -7,21 +7,12 @@ public class PortNumberUtil {
 
   public static int getFreePortNumber(int defaultPortNumber) {
     int port = defaultPortNumber;
-    ServerSocket socket = null;
     try {
-      socket = new ServerSocket(0);
+      ServerSocket socket = new ServerSocket(0);
       port = socket.getLocalPort();
     } catch (IOException e) {
       System.out.println("IOException occured while creating socket."
           + " Use a default port number " +  port);
-    } finally {
-      try {
-        if (socket != null) {
-          socket.close();
-        }
-      } catch (IOException e) {
-        return defaultPortNumber;
-      }
     }
     return port;
   }
--- messaging-client-scribe/src/test/java/com/inmobi/messaging/TestLost.java
+++ messaging-client-scribe/src/test/java/com/inmobi/messaging/TestLost.java
@@ -55,12 +55,13 @@ public class TestLost {
   public void testAckQueueSize() throws Exception {
     NtMultiServer tserver = null;
     try {
-      tserver = new NtMultiServer(new ScribeSlacker(), 0);
+      int port = PortNumberUtil.getFreePortNumber(7946);
+      tserver = new NtMultiServer(new ScribeSlacker(), port);
       tserver.start();
 
       int timeoutSeconds = 2;
       // create publisher with msgqueue size 1
-      ScribeMessagePublisher mb = TestServerStarter.createPublisher(0,
+      ScribeMessagePublisher mb = TestServerStarter.createPublisher(port,
           timeoutSeconds, 1, true, true, 1, 1);
 
       String topic = "retry";
