ceac9837e26729a28624cab93fa76cd11b5aef97
raju.bairishetti <raju.bairishetti@inmobi.com>
2013-08-20 13:44:22 +0530

    partitionCheckpoint will be null in case if consumer started with strat time/start of stream

--- messaging-client-databus/src/main/java/com/inmobi/databus/partition/ClusterReader.java
+++ messaging-client-databus/src/main/java/com/inmobi/databus/partition/ClusterReader.java
@@ -70,19 +70,23 @@ public class ClusterReader extends AbstractPartitionStreamReader {
     PartitionCheckpoint partitioncheckpoint = null;
     Iterator<PartitionCheckpoint> it = partitionCheckpointList.getCheckpoints().
         values().iterator();
-    Date timeStamp = null;
+    Date leastPckTimeStamp = null;
     if (it.hasNext()) {
       partitioncheckpoint = it.next();
-      timeStamp = DatabusStreamWaitingReader.getDateFromCheckpointPath(
-          partitioncheckpoint.getFileName());
+      if (partitioncheckpoint != null) {
+        leastPckTimeStamp = DatabusStreamWaitingReader.getDateFromCheckpointPath(
+            partitioncheckpoint.getFileName());
+      }
     }
     while (it.hasNext()) {
       PartitionCheckpoint tmpPartitionCheckpoint = it.next();
-      Date date = DatabusStreamWaitingReader.getDateFromCheckpointPath(
-          tmpPartitionCheckpoint.getFileName());
-      if (timeStamp.compareTo(date) > 0) {
-        partitioncheckpoint = tmpPartitionCheckpoint;
-        timeStamp = date;
+      if (tmpPartitionCheckpoint != null) {
+        Date pckTimeStamp = DatabusStreamWaitingReader
+            .getDateFromCheckpointPath(tmpPartitionCheckpoint.getFileName());
+        if (pckTimeStamp.before(leastPckTimeStamp)) {
+          partitioncheckpoint = tmpPartitionCheckpoint;
+          leastPckTimeStamp = pckTimeStamp;
+        }
       }
     }
     return partitioncheckpoint;
