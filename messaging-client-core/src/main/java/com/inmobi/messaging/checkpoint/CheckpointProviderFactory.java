9decf8cd4b8e20384c7a479f8043654b0f11444f
Satish Mittal <satish.mittal@inmobi.com>
2013-08-26 12:39:37 +0530

    Move FSCheckpointProvider from messaging-core to messaging-client-databus

--- messaging-client-core/src/main/java/com/inmobi/messaging/checkpoint/CheckpointProviderFactory.java
+++ messaging-client-core/src/main/java/com/inmobi/messaging/checkpoint/CheckpointProviderFactory.java
@@ -15,7 +15,6 @@ package com.inmobi.messaging.checkpoint;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Constructor;
