fc40adb78a2e61b086998d920657ac837a7ca913
Abhishek Agarwal <abhishek.agarwal@inmobi.com>
2015-03-19 15:27:34 +0530

    Review comments

--- messaging-client-core/src/main/java/com/inmobi/messaging/util/GraphiteStatsEmitter.java
+++ messaging-client-core/src/main/java/com/inmobi/messaging/util/GraphiteStatsEmitter.java
@@ -21,19 +21,15 @@ package com.inmobi.messaging.util;
  */
 
 import com.inmobi.messaging.publisher.TopicStatsExposer;
-import com.inmobi.stats.StatsEmitterBase;
 import com.inmobi.stats.StatsExposer;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.Charset;
-import java.util.Date;
 import java.util.Map;
 import java.util.Properties;
 
@@ -41,6 +37,7 @@ public class GraphiteStatsEmitter extends RunnableStatsEmitter {
 
   public static final String METRIC_SEPARATOR = ".";
   public static final String FIELD_SEPARATOR = " ";
+  private static final String NEW_LINE = "\n";
   private static final Log LOG = LogFactory.getLog(GraphiteStatsEmitter.class);
 
   private String metricPrefix;
@@ -50,58 +47,64 @@ public class GraphiteStatsEmitter extends RunnableStatsEmitter {
   @Override
   public void init(Properties props) {
     sleep = Integer.valueOf(props.getProperty("graphite.emitter.poll.interval",
-        "10000"));
+                                              "10000"));
     metricPrefix = props.getProperty("graphite.emitter.metric.prefix", "");
     if (!metricPrefix.isEmpty()) {
       metricPrefix = metricPrefix + METRIC_SEPARATOR;
     }
     graphiteHost = props.getProperty("graphite.emitter.hostname");
-    graphitePort = Integer.parseInt(props.getProperty("graphite.emitter.port"));
+    if (null == graphiteHost || graphiteHost.isEmpty()) {
+      throw new IllegalArgumentException("graphite.emitter.hostname cannot be null or empty");
+    }
+    graphitePort = Integer.parseInt(props.getProperty("graphite.emitter.port", "2003"));
     createThread();
   }
 
   protected void writeStats() {
-    try {
-      if (null != statsExposers) {
-        synchronized (statsExposers) {
-          final StringBuilder lines = new StringBuilder();
-          long timestamp = System.currentTimeMillis();
-          for (StatsExposer exposer : statsExposers) {
-            Map<String, Number> stats = exposer.getStats();
-            Map<String, String> context = exposer.getContexts();
-            String topic = context.get(TopicStatsExposer.TOPIC_CONTEXT_NAME);
-            for (Map.Entry<String, Number> entry : stats.entrySet()) {
-              lines.append(metricPrefix).append(topic).append(METRIC_SEPARATOR)
-                  .append(entry.getKey());
-              lines.append(FIELD_SEPARATOR);
-              lines.append(entry.getValue().longValue());
-              lines.append(FIELD_SEPARATOR);
-              lines.append(timestamp);
-              lines.append("\n");
-            }
+    if (null != statsExposers) {
+      synchronized (statsExposers) {
+        final StringBuilder lines = new StringBuilder();
+        long timestamp = System.currentTimeMillis();
+        for (StatsExposer exposer : statsExposers) {
+          Map<String, Number> stats = exposer.getStats();
+          Map<String, String> context = exposer.getContexts();
+          String topic = context.get(TopicStatsExposer.TOPIC_CONTEXT_NAME);
+          for (Map.Entry<String, Number> entry : stats.entrySet()) {
+            lines.append(metricPrefix).append(topic).append(METRIC_SEPARATOR)
+                .append(entry.getKey());
+            lines.append(FIELD_SEPARATOR);
+            lines.append(entry.getValue().longValue());
+            lines.append(FIELD_SEPARATOR);
+            lines.append(timestamp);
+            lines.append(NEW_LINE);
           }
+        }
 
-          Socket graphiteSocket = null;
-          OutputStream stream = null;
-          try {
-            graphiteSocket = new Socket(graphiteHost, graphitePort);
-            stream = graphiteSocket.getOutputStream();
-            stream.write(lines.toString().getBytes(Charset.forName("UTF-8")));
-          } finally {
-            if (null != stream) {
-              stream.close();
-            }
-            if (null != graphiteSocket && !graphiteSocket.isClosed()) {
+        Socket graphiteSocket = null;
+        OutputStream stream = null;
+        try {
+          graphiteSocket = new Socket(graphiteHost, graphitePort);
+          stream = graphiteSocket.getOutputStream();
+          stream.write(lines.toString().getBytes(Charset.forName("UTF-8")));
+        } catch (IOException ex) {
+          LOG.error("Failed to write the stats", ex);
+        } finally {
+          if (null != graphiteSocket && !graphiteSocket.isClosed()) {
+            try {
               graphiteSocket.close();
+            } catch (IOException ex) {
+              LOG.warn("failure in closing the connection to graphite server", ex);
             }
           }
-
-
+          if (null != stream) {
+            try {
+              stream.close();
+            } catch (IOException ex) {
+              LOG.warn("failure in closing the input stream", ex);
+            }
+          }
         }
       }
-    } catch (IOException ex) {
-      LOG.error("Failed to write the stats", ex);
     }
   }
-
 }
